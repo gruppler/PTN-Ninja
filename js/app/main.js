fbde94c6a7a1f142175ec7bd329532af33f69915
gruppler <gruppler+github@gmail.com>
2017-01-14 13:16:48 -0500

    Don't start in edit mode unless there's an error

--- js/app/main.js
+++ js/app/main.js
@@ -352,11 +352,11 @@ requirejs({locale: navigator.language}, [
   // Set initial mode
   if (config.animate_board) {
     app.$html.removeClass('animate-board');
-    app.toggle_edit_mode(app.$html.hasClass('error') || !location.hash);
+    app.toggle_edit_mode(app.$html.hasClass('error'));
     app.$html.height(); // Update DOM before re-enabling animations
     app.$html.addClass('animate-board');
   } else {
-    app.toggle_edit_mode(app.$html.hasClass('error') || !location.hash);
+    app.toggle_edit_mode(app.$html.hasClass('error'));
   }
   app.restore_caret();
 
