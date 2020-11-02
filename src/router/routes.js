09a1c8d3e102e132198454b8725f15bfd0042275
Craig Laparo <gruppler+github@gmail.com>
2020-11-02 01:16:37 -0500

    Encode game names to prevent URL problems

--- src/router/routes.js
+++ src/router/routes.js
@@ -25,7 +25,7 @@ const routes = [
       }
 
       if (state.name) {
-        name = state.name;
+        name = decompressFromEncodedURIComponent(state.name) || state.name;
         delete state.name;
       }
 
--- src/store/ui/getters.js
+++ src/store/ui/getters.js
@@ -92,9 +92,9 @@ export const url = state => (game, options = {}) => {
   let params = {};
 
   if ("name" in options) {
-    params.name = options.name;
+    params.name = compressToEncodedURIComponent(options.name);
   } else if (game.name && game.name !== game.generateName()) {
-    params.name = game.name;
+    params.name = compressToEncodedURIComponent(game.name);
   }
 
   if (options.origin) {
