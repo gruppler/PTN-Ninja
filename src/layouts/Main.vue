43ba760df693097d0970eb3ce1cff14d668a0322
Craig Laparo <gruppler+github@gmail.com>
2020-12-05 00:27:42 -0500

    Performance improvement

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -47,7 +47,7 @@
           @shortkey="miscShortkey"
         >
           <Board ref="board" class="col-grow" :game="game" />
-          <smooth-reflow
+          <div
             @click.right.self.prevent="$refs.board.resetBoardRotation"
             class="board-move-container"
           >
@@ -62,7 +62,7 @@
               current-only
               standalone
             />
-          </smooth-reflow>
+          </div>
         </div>
         <q-page-sticky position="bottom-right" :offset="[18, 18]">
           <Menu @input="menuAction" @click.right.prevent="switchGame" />
