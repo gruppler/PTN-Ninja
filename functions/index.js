b9dae59a0588fec097e011e543b450720f26f0a9
Craig Laparo <gruppler+github@gmail.com>
2020-12-06 17:45:59 -0500

    Include TPS Ninja

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
-const functions = require("firebase-functions");
+"use strict";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const { TPStoCanvas } = require("./TPS-Ninja/src");
+
+exports.tps = functions.https.onRequest((request, response) => {
+  response.setHeader("Content-Type", "image/png");
+  response.setHeader(
+    "Content-Disposition",
+    `attachment; filename="${request.query.name || "tps"}.png"`
+  );
+  TPStoCanvas(request.query)
+    .pngStream()
+    .pipe(response);
+});
