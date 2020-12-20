b6b35ef63746930dade63dc0ca88d3704e8fe347
Craig Laparo <gruppler+github@gmail.com>
2020-12-19 20:09:04 -0500

    Remove seamlessness of Close/Download Games dialogs

--- src/components/dialogs/DownloadGames.vue
+++ src/components/dialogs/DownloadGames.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-dialog :value="value" @input="$emit('input', $event)" seamless>
+  <q-dialog :value="value" @input="$emit('input', $event)">
     <q-card style="width: 400px; overflow: visible" class="bg-secondary">
       <dialog-header icon="download"> {{ $t("Download") }}... </dialog-header>
 
