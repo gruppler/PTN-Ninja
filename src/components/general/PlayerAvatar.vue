72e8908410a9d642811775800ac2684ddbfc3b2e
Craig Laparo <gruppler+github@gmail.com>
2023-02-03 23:23:06 -0500

    Replace player identicon dependency

--- src/components/general/PlayerAvatar.vue
+++ src/components/general/PlayerAvatar.vue
@@ -1,7 +1,7 @@
 <template>
   <q-avatar
     v-if="value"
-    class="player-avatar"
+    class="player-avatar rounded-borders"
     v-html="icon"
     v-bind="$attrs"
     v-on="$listeners"
@@ -10,16 +10,37 @@
 </template>
 
 <script>
-import { toSvg } from "jdenticon";
+import Identicon from "identicon.js";
+import sha1 from "sha1";
+import { isNumber } from "lodash";
+
+const SIZES = {
+  xs: 18,
+  sm: 24,
+  md: 32,
+  lg: 38,
+  xl: 46,
+};
 
 export default {
   name: "PlayerAvatar",
   props: {
     value: String,
+    size: {
+      type: [Number, String],
+      default: 48,
+    },
   },
   computed: {
     icon() {
-      return toSvg(this.value.trim().toLowerCase(), "100");
+      const size = isNumber(this.size)
+        ? this.size
+        : SIZES[this.size] || SIZES.md;
+      const data = new Identicon(sha1(this.value.trim().toLowerCase()), {
+        background: [0, 0, 0, 192],
+        size,
+      }).toString();
+      return `<img width=${size} height=${size} src="data:image/png;base64,${data}" />`;
     },
   },
 };
