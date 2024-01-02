50899a30c77de98cbfc9ca71ad63d53b7dd7f3b4
Craig Laparo <gruppler+github@gmail.com>
2024-01-02 12:04:24 -0600

    Bug fixes

--- src/store/game/state.js
+++ src/store/game/state.js
@@ -1,5 +1,6 @@
 import { LocalStorage, Platform } from "quasar";
 import Game from "../../Game";
+import { uniq } from "lodash";
 
 const state = {
   error: null,
@@ -20,7 +21,7 @@ const load = (key, initial) =>
   LocalStorage.has(key) ? LocalStorage.getItem(key) : initial;
 
 if (!Platform.within.iframe && LocalStorage.has("games")) {
-  state.list = LocalStorage.getItem("games").map((name) => {
+  state.list = uniq(LocalStorage.getItem("games")).map((name) => {
     const ptn = load("ptn-" + name);
     let state = load("state-" + name);
     if (ptn && (!state || !state.tps || !("ply" in state))) {
