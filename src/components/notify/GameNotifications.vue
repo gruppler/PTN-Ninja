62a10d48f17cb657877ea3b2044180c5f53befe3
Craig Laparo <gruppler+github@gmail.com>
2024-01-03 11:25:32 -0600

    Fix regression

--- src/components/notify/GameNotifications.vue
+++ src/components/notify/GameNotifications.vue
@@ -28,7 +28,8 @@ export default {
           alerts.push({
             message: this.$t("result." + result.type, {
               player:
-                this.game.tags["player" + winner] || this.$t("Player" + winner),
+                this.game.ptn.tags["player" + winner] ||
+                this.$t("Player" + winner),
             }),
             player: winner,
           });
