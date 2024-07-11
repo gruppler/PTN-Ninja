76b50f1d853ddbf6c72409c42fa1eb226fa29dfd
Craig Laparo <gruppler+github@gmail.com>
2024-07-11 00:07:31 -0500

    Disable share hotkeys (Ctrl+C, etc.) when text is selected

--- src/components/controls/ShareButton.vue
+++ src/components/controls/ShareButton.vue
@@ -4,7 +4,7 @@
     v-bind="$attrs"
     @click="noMenu ? share() : null"
     @click.right.prevent="share"
-    v-shortkey="hotkeys"
+    v-shortkey="isTextSelected ? null : hotkeys"
     @shortkey="shortkey"
   >
     <hint>{{ $t("Share") }}</hint>
@@ -32,6 +32,8 @@
 
 <script>
 import { HOTKEYS } from "../../keymap";
+import { useTextSelection } from "@vueuse/core";
+const selection = useTextSelection();
 
 export default {
   name: "ShareButton",
@@ -45,6 +47,9 @@ export default {
     };
   },
   computed: {
+    isTextSelected() {
+      return Boolean(selection.text.value);
+    },
     actions() {
       let actions = [
         {
