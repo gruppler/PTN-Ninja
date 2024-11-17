8ab1571709ad6e60e2c762e1995805c2e6719257
Craig Laparo <gruppler+github@gmail.com>
2024-11-16 21:04:01 -0600

    Bug fix

--- src/components/drawers/BotSuggestions.vue
+++ src/components/drawers/BotSuggestions.vue
@@ -665,7 +665,7 @@ export default {
             console.error("Failed to query position", {
               tps,
               komi,
-              secondsToThink,
+              secondsToThinkPerPly,
               error,
             });
           })
