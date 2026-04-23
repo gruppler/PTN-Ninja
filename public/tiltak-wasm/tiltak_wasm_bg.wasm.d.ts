df2731dc533f1e9410f9aacd5d5b84d2df8c2cd6
Craig Laparo <gruppler+github@gmail.com>
2026-04-23 00:24:35 -0500

    Update tiltak-wasm bundle with MultiPV fix for go nodes
    Rebuilt from tiltak@b1d35de2 which unifies MultiPV info emission across
    all go variants. Previously the `go nodes` branch ignored
    options.multi_pv, so MultiPV info lines only appeared in interactive
    analysis (which uses `go infinite`). This bundle emits multipv info
    for analyze position / branch / game as well.

--- public/tiltak-wasm/tiltak_wasm_bg.wasm.d.ts
+++ public/tiltak-wasm/tiltak_wasm_bg.wasm.d.ts
@@ -1,19 +1,15 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export const annotate_ptn: (a: number, b: number, c: number) => [number, number, number, number];
-export const is_tak: (a: number, b: number, c: number) => [number, number, number];
-export const is_tinue: (a: number, b: number, c: number, d: number) => [number, number, number];
 export const start_engine: (a: any) => any;
 export const __wbindgen_exn_store: (a: number) => void;
 export const __externref_table_alloc: () => number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export const __wbindgen_export_3: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-export const __externref_table_dealloc: (a: number) => void;
-export const __wbindgen_free: (a: number, b: number, c: number) => void;
-export const _dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hde031b271b30aafa: (a: number, b: number, c: number, d: number) => void;
-export const closure141_externref_shim: (a: number, b: number, c: any) => void;
-export const closure177_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure62_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb7ab370a839176df: (a: number, b: number, c: number, d: number) => void;
+export const closure27_externref_shim: (a: number, b: number, c: any, d: any) => void;
 export const __wbindgen_start: () => void;
